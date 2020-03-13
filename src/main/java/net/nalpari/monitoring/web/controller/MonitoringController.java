8c70e01d05badd867ab1caa58ebab60226907470
nalpari <yoosw@3top.co.kr>
2020-03-13 15:43:29 +0900

    initial commit

---
+++ src/main/java/net/nalpari/monitoring/web/controller/MonitoringController.java
@@ -0,0 +1,27 @@
+package net.nalpari.monitoring.web.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import net.nalpari.monitoring.web.model.Sample;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class MonitoringController {
+
+    @GetMapping(value = {"", "/"})
+    public Sample index() {
+        Sample result = new Sample();
+        result.setId("1");
+        result.setMessage("result");
+        return result;
+    }
+
+    @GetMapping("/long-process")
+    public String pause() throws InterruptedException {
+        log.info("#### thread start");
+        Thread.sleep(10000);
+        log.info("#### thread end");
+        return "Process finished";
+    }
+}
---
+++ src/main/java/net/nalpari/monitoring/web/model/Sample.java
@@ -0,0 +1,11 @@
+package net.nalpari.monitoring.web.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Sample {
+    private String id;
+    private String message;
+}
